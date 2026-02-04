d3d743f947797927bb44d1120b39742f12301127
Sascha Brawer <sascha@brawer.ch>
2026-02-04 12:16:38 +0100

    Add unit test for converting Field to ParseValue
    Before this change, the code in `field.rs` was not covered by tests.

--- src/field.rs
+++ src/field.rs
@@ -11,3 +11,20 @@ impl<'a> From<Field<'a>> for ParseValue<'a> {
         m.value
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::parse::parse_field;
+    use crate::varint::Varint;
+
+    #[test]
+    fn test_parse_value_from_field() {
+        let data = [0x08, 0x96, 0x01];
+        let (field, _rest) = parse_field(&data).unwrap();
+        assert_eq!(
+            ParseValue::from(field),
+            ParseValue::Varint(Varint { value: 150 })
+        );
+    }
+}
